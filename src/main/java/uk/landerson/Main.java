7cc404b4616bbb864882aecae8de8e3fd2a62998
Luke Anderson <landerson61@gmail.com>
2022-10-31 11:41:48 +0000

    refactor: move mortgage calculation to own method
    Refactor the mortgage calculation into its own method to make the code more readable.

--- src/main/java/uk/landerson/Main.java
+++ src/main/java/uk/landerson/Main.java
@@ -10,49 +10,52 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
-
-        int loanPrincipal;
         final int MIN_LOAN_PRINCIPAL = 1000;
         final int MAX_LOAN_PRINCIPAL = 1_000_000;
-        final String PRINCIPAL_PROMPT = "Principal (£1k - £1M): ";
+
+        int loanPrincipal;
 
         do {
-            System.out.print(PRINCIPAL_PROMPT);
+            System.out.print("Principal (£1k - £1M): ");
             loanPrincipal = scanner.nextInt();
         } while (loanPrincipal < MIN_LOAN_PRINCIPAL || loanPrincipal > MAX_LOAN_PRINCIPAL);
 
         float yearlyInterest;
         final byte MIN_INTEREST_PERCENT = 0;
         final byte MAX_INTEREST_PERCENT = 30;
-        final String INTEREST_PROMPT = "Annual interest rate (%) (0 - 30): ";
 
         do {
-            System.out.print(INTEREST_PROMPT);
+            System.out.print("Annual interest rate (%) (0 - 30): ");
             yearlyInterest = scanner.nextFloat();
         } while (yearlyInterest < MIN_INTEREST_PERCENT || yearlyInterest > MAX_INTEREST_PERCENT);
 
         float yearlyInterestPercent = yearlyInterest / PERCENT;
-        float monthlyInterestPercent = yearlyInterestPercent / MONTHS_IN_YEAR;
 
         byte termInYears;
         final byte MIN_TERM_YEARS = 1;
         final byte MAX_TERM_YEARS = 30;
-        final String TERM_PROMPT = "Loan terms in years (1 - 30): ";
 
         do {
-            System.out.print(TERM_PROMPT);
+            System.out.print("Loan terms in years (1 - 30): ");
             termInYears = scanner.nextByte();
         } while (termInYears < MIN_TERM_YEARS || termInYears > MAX_TERM_YEARS);
 
-        int termInMonths = termInYears * MONTHS_IN_YEAR;
+        double monthlyRepayment = caclulateMortgage(loanPrincipal, yearlyInterestPercent, termInYears);
+        String formattedRepayment = NumberFormat.getCurrencyInstance().format(monthlyRepayment);
+        System.out.println("Mortgage: " + formattedRepayment);
+    }
 
-        double monthlyRepayment = loanPrincipal
+    public static double caclulateMortgage(int principal, float yearlyInterest, int years) {
+
+        final byte MONTHS_IN_YEAR = 12;
+
+        float monthlyInterestPercent = yearlyInterest / MONTHS_IN_YEAR;
+        int termInMonths = years * MONTHS_IN_YEAR;
+
+        return principal
                 * (((monthlyInterestPercent * Math.pow((1 + monthlyInterestPercent), termInMonths))
                 / (Math.pow(1 + monthlyInterestPercent, termInMonths) - 1)));
 
-        String formattedRepayment = NumberFormat.getCurrencyInstance().format(monthlyRepayment);
-        System.out.println("Mortgage: " + formattedRepayment);
     }
 }
