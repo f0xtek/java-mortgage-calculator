b5cdf4d940797d850fd88cf5f8050d02fda5a462
Luke Anderson <landerson61@gmail.com>
2022-10-31 11:36:21 +0000

    refactor: Add input validation
    Validate that user input values fall within the expected ranges

--- src/main/java/uk/landerson/Main.java
+++ src/main/java/uk/landerson/Main.java
@@ -7,21 +7,46 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
 
-        System.out.print("Principal: ");
-        int loanPrincipal = scanner.nextInt();
+        int loanPrincipal;
+        final int MIN_LOAN_PRINCIPAL = 1000;
+        final int MAX_LOAN_PRINCIPAL = 1_000_000;
+        final String PRINCIPAL_PROMPT = "Principal (£1k - £1M): ";
 
-        System.out.print("Annual interest rate (%): ");
-        float monthlyInterestPercent = (scanner.nextFloat() / PERCENT) / MONTHS_IN_YEAR;
+        do {
+            System.out.print(PRINCIPAL_PROMPT);
+            loanPrincipal = scanner.nextInt();
+        } while (loanPrincipal < MIN_LOAN_PRINCIPAL || loanPrincipal > MAX_LOAN_PRINCIPAL);
 
-        System.out.print("Loan terms in years: ");
-        int termInMonths = scanner.nextByte() * MONTHS_IN_YEAR;
+        float yearlyInterest;
+        final byte MIN_INTEREST_PERCENT = 0;
+        final byte MAX_INTEREST_PERCENT = 30;
+        final String INTEREST_PROMPT = "Annual interest rate (%) (0 - 30): ";
+
+        do {
+            System.out.print(INTEREST_PROMPT);
+            yearlyInterest = scanner.nextFloat();
+        } while (yearlyInterest < MIN_INTEREST_PERCENT || yearlyInterest > MAX_INTEREST_PERCENT);
+
+        float yearlyInterestPercent = yearlyInterest / PERCENT;
+        float monthlyInterestPercent = yearlyInterestPercent / MONTHS_IN_YEAR;
+
+        byte termInYears;
+        final byte MIN_TERM_YEARS = 1;
+        final byte MAX_TERM_YEARS = 30;
+        final String TERM_PROMPT = "Loan terms in years (1 - 30): ";
+
+        do {
+            System.out.print(TERM_PROMPT);
+            termInYears = scanner.nextByte();
+        } while (termInYears < MIN_TERM_YEARS || termInYears > MAX_TERM_YEARS);
+
+        int termInMonths = termInYears * MONTHS_IN_YEAR;
 
         double monthlyRepayment = loanPrincipal
                 * (((monthlyInterestPercent * Math.pow((1 + monthlyInterestPercent), termInMonths))
@@ -30,5 +55,4 @@ public class Main {
         String formattedRepayment = NumberFormat.getCurrencyInstance().format(monthlyRepayment);
         System.out.println("Mortgage: " + formattedRepayment);
     }
-
 }
