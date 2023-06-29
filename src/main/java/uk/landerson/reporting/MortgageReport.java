8279c1f282d7de1f0e4140794c56915bd33f8de7
Luke Anderson <landerson61@gmail.com>
2023-06-29 09:46:33 +0100

    refactor: Extract currency instance into field
    Removes an unnecessary method.

--- src/main/java/uk/landerson/reporting/MortgageReport.java
+++ src/main/java/uk/landerson/reporting/MortgageReport.java
@@ -7,9 +7,11 @@ import java.text.NumberFormat;
 public class MortgageReport {
 
     private final MortgageCalculator calculator;
+    private final NumberFormat currency;
 
     public MortgageReport(MortgageCalculator calculator) {
         this.calculator = calculator;
+        currency = NumberFormat.getCurrencyInstance();
     }
 
     public void printPaymentPlan() {
@@ -17,20 +19,16 @@ public class MortgageReport {
         System.out.println("Payment plan:");
 
         for(double balance : calculator.getRemainingBalances()) {
-            System.out.println(convertToCurrency(balance));
+            System.out.println(currency.format(balance));
         }
     }
 
     public void printMonthlyRepayment() {
         printHeader("MORTGAGE");
-        System.out.printf("Monthly Payment: %s\n", convertToCurrency(calculator.calculateMonthlyPayment()));
+        System.out.printf("Monthly Payment: %s\n", currency.format(calculator.calculateMonthlyPayment()));
     }
 
     private void printHeader(String message) {
         System.out.printf("\n%s\n%s\n", message, "-".repeat(message.length()));
-    }
-
-    private String convertToCurrency(double amount) {
-        return NumberFormat.getCurrencyInstance().format(amount);
     }
 }
